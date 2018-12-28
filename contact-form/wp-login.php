b456855c94f601ef866f66bd798a3059129c9e31
Dilshat Hewzulla <hewzulla@gmail.com>
2018-12-28 00:40:20 +0000

    scaling the server

--- contact-form/wp-login.php
+++ contact-form/wp-login.php
@@ -1100,71 +1100,76 @@ try {
 
 
 	var globalinput={
-						 api:require("global-input-message")
+						 api:require("global-input-message"),
+						 connector:null,
+						 connectToGlobalInputApp:function(){
+							 				this.connector=globalinput.api.createMessageConnector();
+						 					this.connector.connect(globalinput.config);
+						 },
+						 config:{
+							 				 url:"https://globalinput.co.uk",
+											 apikey:"k7jc3QcMPKEXGW5UC",
+											 onSenderConnected:function(){
+													qrCodeElement.style.display="none";
+											 },
+											 onSenderDisconnected:function(){
+													qrCodeElement.style.display="block";
+											 },
+											 onRegistered:function(next){
+												 	next();
+													this.displayQRCode();
+											 },
+											 displayQRCode:function(){
+												 var codedata=this.connector.buildInputCodeData();
+												 var qrcode = new QRCode(qrCodeElement, {
+												 	text: codedata,
+												 	width: 300,
+												 	height: 300,
+												 	colorDark : "#000000",
+												 	colorLight : "#ffffff",
+												 	correctLevel : QRCode.CorrectLevel.H
+												 });
+											 },
+											 initData:{
+														action:"input",
+														dataType:"form",
+													 form:{
+														 id:  "###username###@"+window.location.hostname+".wordpress",
+														 title: "Wordpress login",
+														 fields:[{
+																			 label:"Username",
+																			 id:"username",
+																			 operations:{
+																					 onInput:function(username){
+																								document.getElementById("user_login").value=username;
+																					 }
+																			 }
+
+																		 },{
+																				label:"Password",
+																				id:"password",
+																				type:"secret",
+																				operations:{
+																					onInput:function(password){
+																					 document.getElementById("user_pass").value=password;
+																					}
+																				}
+
+																		 },{
+																				label:"Login",
+																				type:"button",
+																				operations:{
+																					 onInput:function(){
+																									 document.getElementById("wp-submit").click();
+																					 }
+																				}
+
+																		 }]
+																 }
+													 }
+						 }
 				 };
-
-
-				 globalinput.config={
-																				 onSenderConnected:function(){
-																					 	qrCodeElement.style.display="none";
-																				 },
-																				 onSenderDisconnected:function(){
-																					 	qrCodeElement.style.display="block";
-																				 },
-																				 initData:{
-																					 		action:"input",
-                                							dataType:"form",
-																						 form:{
-																							 id:  "###username###@"+window.location.hostname+".wordpress",
-																							 title: "Wordpress login",
-																							 fields:[{
-																												 label:"Username",
-																												 id:"username",
-																												 operations:{
-																														 onInput:function(username){
-																																	document.getElementById("user_login").value=username;
-																														 }
-																												 }
-
-																											 },{
-																													label:"Password",
-																													id:"password",
-																													type:"secret",
-																													operations:{
-																														onInput:function(password){
-																														 document.getElementById("user_pass").value=password;
-																														}
-																													}
-
-																											 },{
-																													label:"Login",
-																													type:"button",
-																													operations:{
-																														 onInput:function(){
-																																		 document.getElementById("wp-submit").click();
-																														 }
-																													}
-
-																											 }]
-																									 }
-																						 }
-
-																		 };
-						globalinput.connector=globalinput.api.createMessageConnector();
-						globalinput.connector.connect(globalinput.config);
-						var codedata=globalinput.connector.buildInputCodeData();
-
-
-
-						var qrcode = new QRCode(qrCodeElement, {
-							text: codedata,
-							width: 300,
-							height: 300,
-							colorDark : "#000000",
-							colorLight : "#ffffff",
-							correctLevel : QRCode.CorrectLevel.H
-						});
-
+				 globalinput.globalinput();
 })();
 
 </script>
--- wp-globalinput-login/assets/js/GlobalInput_WPLogIn.js
+++ wp-globalinput-login/assets/js/GlobalInput_WPLogIn.js
@@ -15,71 +15,84 @@
 
 	formElement.parentNode.insertBefore(wrap,formElement);
 	var globalinput={
-						api:require("global-input-message")
+						api:require("global-input-message"),
+						connectToGlobalInputApp:function(){
+								this.connector=globalinput.api.createMessageConnector();
+								this.connector.connect(this.config);
+						},
+						displayQRCode:function(){
+							var codedata=this.connector.buildInputCodeData();
+							var qrcode = new QRCode(qrCodeElement, {
+																	text: codedata,
+																	width: 300,
+																	height: 300,
+																	colorDark : "#000000",
+																	colorLight : "#ffffff",
+																	correctLevel : QRCode.CorrectLevel.H
+							});
+						},
+						config:{
+
+												onSenderConnected:function()
+																	{
+																		wrap.style.display="none";
+																	},
+												onSenderDisconnected:function()
+																		{
+																			wrap.style.display="block";
+																		},
+												url:"https://globalinput.co.uk", //URL to your Global Input WebSocket Server
+												apikey:"k7jc3QcMPKEXGW5UC",      //API Key for connecting to the Global Input Server
+												connector:null,
+												onRegistered:function(next){
+                                    next();
+                                    globalinput.displayQRCode();
+                       },
+												initData:{
+															action:"input",
+															dataType:"form",
+															form:{
+																	id:  "###username###@"+window.location.hostname+".wordpress",
+																	title: "Wordpress login",
+																	fields:[{
+																			label:"Username",
+																			id:"username",
+																			operations:
+																				{
+																					onInput:function(username)
+																					{
+																						document.getElementById("user_login").value=username;
+																					}
+																				}
+																			},
+																			{
+																				label:"Password",
+																				id:"password",
+																				type:"secret",
+																				operations:
+																				{
+																					onInput:function(password)
+																					{
+																						document.getElementById("user_pass").value=password;
+																					}
+																				}
+																			},
+																			{
+																				label:"Login",
+																				type:"button",
+																				operations:
+																					{
+																						onInput:function()
+																						{
+																							document.getElementById("wp-submit").click();
+																						}
+																					}
+																			}]
+																	}
+															}
+						}
+
 					};
-	globalinput.config={
-						onSenderConnected:function()
-											{
-												wrap.style.display="none";
-											},
-						onSenderDisconnected:function()
-												{
-													wrap.style.display="block";
-												},
-						url:"https://globalinput.co.uk", //URL to your Global Input WebSocket Server
-						apikey:"k7jc3QcMPKEXGW5UC",      //API Key for connecting to the Global Input Server
-						initData:{
-									action:"input",
-									dataType:"form",
-									form:{
-											id:  "###username###@"+window.location.hostname+".wordpress",
-											title: "Wordpress login",
-											fields:[{
-													label:"Username",
-													id:"username",
-													operations:
-														{
-															onInput:function(username)
-															{
-																document.getElementById("user_login").value=username;
-															}
-														}
-													},
-													{
-														label:"Password",
-														id:"password",
-														type:"secret",
-														operations:
-														{
-															onInput:function(password)
-															{
-																document.getElementById("user_pass").value=password;
-															}
-														}
-													},
-													{
-														label:"Login",
-														type:"button",
-														operations:
-															{
-																onInput:function()
-																{
-																	document.getElementById("wp-submit").click();
-																}
-															}
-													}]
-											}
-									}
-						};
-						globalinput.connector=globalinput.api.createMessageConnector();
-						globalinput.connector.connect(globalinput.config);
-						var codedata=globalinput.connector.buildInputCodeData();
-						var qrcode = new QRCode(qrCodeElement, {
-																text: codedata,
-																width: 300,
-																height: 300,
-																colorDark : "#000000",
-																colorLight : "#ffffff",
-																correctLevel : QRCode.CorrectLevel.H
-															});
+					globalinput.connectToGlobalInputApp();
+
 })();
